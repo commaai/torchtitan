3e5390a10b52f824672b3accce60ea910fb42a40
YassineYousfi <yyousfi1@binghamton.edu>
2026-06-22 20:06:58 -0700

    no pretrain

--- torchtitan/experiments/path/config_registry.py
+++ torchtitan/experiments/path/config_registry.py
@@ -190,7 +190,7 @@ def _model_config(flavor: str) -> PathModel.Config:
             input_frame_names=tuple(input_frame_names),
             in_channels=in_channels,
             vision_features=vision_features,
-            pretrained=flavor == "fastvit_t12",
+            pretrained=False,
             drop_path_rate=0.2,
             mean=255 / 2,
             std=255 / 4,
