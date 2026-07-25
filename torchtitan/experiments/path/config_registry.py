d7b979d1ab93d8e6bca82066e2cbe09b6ac23250
YassineYousfi <yyousfi1@binghamton.edu>
2026-07-25 14:56:17 -0700

    lt

--- torchtitan/experiments/path/config_registry.py
+++ torchtitan/experiments/path/config_registry.py
@@ -9,7 +9,7 @@ from __future__ import annotations
 import math
 import os
 from functools import partial
-from xx.datasets.constants import BASE_DIR_GT, BASE_DIR_GT_10M, DEFAULT_TEST_5K_LIST_TAGGED, DEFAULT_BIG_TRAIN_LIST
+from xx.datasets.constants import BASE_DIR_GT, BASE_DIR_GT_10M, DEFAULT_BIG_TRAIN_LIST, DEFAULT_TEST_5K_LIST_TAGGED
 from xx.ml_tools.constants.model import (
     frame_constants_from_fps,
     FRAME_TYPE,
